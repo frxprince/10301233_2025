1d91188810badc3304e58c26516fa350eabed7f6
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-12-24 11:44:45 +0700

    ...

--- kotlin/hellokotlin/src/main/kotlin/kotlinday4.kt
+++ kotlin/hellokotlin/src/main/kotlin/kotlinday4.kt
@@ -29,14 +29,29 @@ class AnyShape(var width_any:Int,var height_any:Int):Rectangle(width_any,height_
     }
 }
 
+class AnyShapeLambda(var width_any:Int,var height_any:Int):Rectangle(width_any,height_any){
+    init { println("The Anyshape Class just created!")  }
+    private  var area=0
+    fun printAnyArea(){println("The area of AnyShape is $area")}
+    fun CalArea(Area:(w:Int,h:Int)->Int){
+        area=Area(width,height_rec)
+    }
+}
 fun main(){
-var Diamondobj=AnyShape(10,30)
+    var Diamondobj2=AnyShapeLambda(10,20)
+    Diamondobj2.printwidth();Diamondobj2.printHeight()
+    Diamondobj2.CalArea({w,h-> (w*h)/2})
+    Diamondobj2.printAnyArea()
+/*var Diamondobj=AnyShape(10,30)
  Diamondobj.printwidth();Diamondobj.printHeight()
  Diamondobj.CalArea(object:findAreaAnyShape{
      override fun CalArea(w: Int, h: Int): Int {
         return (w*h)/2
      } })
  Diamondobj.printAnyArea()
+
+   */
+
 //  var RecObj= Rectangle(10,20)
  //  RecObj.printwidth();RecObj.printHeight();RecObj.printArea()
  //val LineAobj= Line(5);LineAobj.printwidth();val LineBobj=Line(10)
