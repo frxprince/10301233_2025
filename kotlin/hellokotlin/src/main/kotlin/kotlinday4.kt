8ac6461e8a07e1024570be584a043ad240e3376d
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2025-12-24 11:28:47 +0700

    ...

--- kotlin/hellokotlin/src/main/kotlin/kotlinday4.kt
+++ kotlin/hellokotlin/src/main/kotlin/kotlinday4.kt
@@ -9,15 +9,40 @@ open class Line(var width: Int=0){
 
 open class Square(var width_square:Int):Line(width_square){
     init { println("The Square Class just created!")  }
-    fun printArea(){ println("The area of this Square is ${width*width}")      }
+  open  fun printArea(){ println("The area of this Square is ${width*width}")      }
 }
-class Rectangle()
+open class Rectangle(var width_rec:Int,var height_rec:Int):Square(width_rec){
+    init { println("The Rectangle Class just created!")  }
+    fun printHeight(){println("The height of this Rectangle is $height_rec")}
+    override fun printArea(){ println("The area of this Rectangle is ${width*height_rec}")      }
 
+}
+interface findAreaAnyShape{
+    fun CalArea(w:Int,h:Int):Int
+}
+class AnyShape(var width_any:Int,var height_any:Int):Rectangle(width_any,height_any){
+    init { println("The Anyshape Class just created!")  }
+    private  var area=0
+    fun printAnyArea(){println("The area of AnyShape is $area")}
+    fun CalArea(Area:findAreaAnyShape){
+      area=  Area.CalArea(width,height_rec)
+    }
+}
 
 fun main(){
+var Diamondobj=AnyShape(10,30)
+ Diamondobj.printwidth();Diamondobj.printHeight()
+ Diamondobj.CalArea(object:findAreaAnyShape{
+     override fun CalArea(w: Int, h: Int): Int {
+        return (w*h)/2
+     } })
+ Diamondobj.printAnyArea()
+//  var RecObj= Rectangle(10,20)
+ //  RecObj.printwidth();RecObj.printHeight();RecObj.printArea()
  //val LineAobj= Line(5);LineAobj.printwidth();val LineBobj=Line(10)
  //LineBobj.printwidth();  LineAobj.printwidth()
-val SquareObj =Square(15)
-SquareObj.printwidth();SquareObj.printArea()
+//val SquareObj =Square(15)
+//SquareObj.printwidth();SquareObj.printArea()
+
 
 }
