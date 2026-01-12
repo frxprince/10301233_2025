a99e46a7f9978167887dce978bec0309deac1fb0
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2026-01-12 16:13:32 +0700

    ....

--- android/lenghtconversion1/app/src/main/java/com/example/lenghtconversion1/MainActivity.kt
+++ android/lenghtconversion1/app/src/main/java/com/example/lenghtconversion1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lenghtconversion1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+val TxtInput=findViewById<EditText>(R.id.TxtInput)
+val BtnInchToCm=findViewById<Button>(R.id.BtnInchtoCm)
+val BtnCmToInch=findViewById<Button>(R.id.BtnCmtoInch)
+val TxtOutput=findViewById<TextView>(R.id.Txtoutput)
+ BtnInchToCm.setOnClickListener {
+     var ans:Double=  TxtInput.text.toString().toDouble()*2.54
+  TxtOutput.setText(ans.toString()+" cm")
+ }
+BtnCmToInch.setOnClickListener {  // printf("%.2f",ans)
+    TxtOutput.setText ("%.2f".format(TxtInput.text.toString().toDouble()/2.54)+
+    " inch")  }
     }
 }
