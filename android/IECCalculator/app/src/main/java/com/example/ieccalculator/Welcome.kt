e8c94bc4fa65546a2eeede9c6d3f26c7fb7b49f1
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2026-01-14 11:56:55 +0700

    ...

---
+++ android/IECCalculator/app/src/main/java/com/example/ieccalculator/Welcome.kt
@@ -0,0 +1,27 @@
+package com.example.ieccalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+val BtnNext=findViewById<ImageButton>(R.id.BtnNext)
+BtnNext.setOnClickListener {
+val i= Intent(this, MainActivity::class.java)
+startActivity(i)
+}
+    }
+}
