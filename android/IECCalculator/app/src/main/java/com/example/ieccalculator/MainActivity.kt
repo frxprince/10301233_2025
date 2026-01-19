905178b4ff27234e11120d4098d06d39a55621c5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2026-01-19 16:46:19 +0700

    ...

--- android/IECCalculator/app/src/main/java/com/example/ieccalculator/MainActivity.kt
+++ android/IECCalculator/app/src/main/java/com/example/ieccalculator/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.ieccalculator
 
+import android.content.Intent
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +23,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+ val TxtInput=findViewById<EditText>(R.id.TxtInput)
+ val BtnInputUnit=findViewById<Button>(R.id.BtnInputUnit)
+ val TxtBase=findViewById<TextView>(R.id.TxtBase)
+ val BtnOutputUnit=findViewById<Button>(R.id.BtnOutputUnit)
+ var BaseValue:Double=0.0
+ val InputRes=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+     result: ActivityResult->
+      if(result.resultCode == RESULT_OK){
+   //    TxtBase.setText(result.data?.getStringExtra("prefix")?:"no data")
+   BaseValue=when(result.data!!.getStringExtra("prefix")){
+      "kB"-> TxtInput.text.toString().toDouble() * Math.pow(10.0,3.0)
+      "MB"-> TxtInput.text.toString().toDouble() * Math.pow(10.0,6.0)
+      "GB"-> TxtInput.text.toString().toDouble() * Math.pow(10.0,9.0)
+      "TB"-> TxtInput.text.toString().toDouble() * Math.pow(10.0,12.0)
+      else-> 0.0
+  }
+  TxtBase.setText(DecimalFormat("#,###,###.###").format(BaseValue) +" Bytes")
+      }
+ }
+BtnInputUnit.setOnClickListener { val i= Intent(this, InputPrefiix::class.java)
+ InputRes.launch(i)
+}
+
+
     }
 }
