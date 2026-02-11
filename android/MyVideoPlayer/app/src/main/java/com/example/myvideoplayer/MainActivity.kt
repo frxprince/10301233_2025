4c46952571486315ca6094d1e2bbf0760d88ea2f
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2026-02-11 11:22:39 +0700

    ...

--- android/MyVideoPlayer/app/src/main/java/com/example/myvideoplayer/MainActivity.kt
+++ android/MyVideoPlayer/app/src/main/java/com/example/myvideoplayer/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myvideoplayer
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+val videoView=findViewById<VideoView>(R.id.videoView)
+val btnLoadResource=findViewById<Button>(R.id.BtnResource)
+val btnLoadInternet=findViewById<Button>(R.id.BtnInternet)
+val btnLoadSDCARD=findViewById<Button>(R.id.BtnSDCARD)
+val btnPlay=findViewById<Button>(R.id.BtnPlay)
+val btnPause=findViewById<Button>(R.id.BtnPause)
+val btnStop=findViewById<Button>(R.id.BtnStop)
+btnLoadResource.setOnClickListener {
+videoView.setVideoURI(Uri.parse("android.resource://"+packageName+"/"+R.raw.video2 ))
+}
+videoView.setMediaController(MediaController(this))
+btnPlay.setOnClickListener { videoView.start() }
+ btnStop.setOnClickListener { videoView.stopPlayback() }
+ btnPause.setOnClickListener { videoView.pause() }
+ btnLoadInternet.setOnClickListener {
+     videoView.setVideoURI(
+         Uri.parse("http://www.drpaween.com/ohm/cs436/mv.mp4"))
+ }
     }
 }
